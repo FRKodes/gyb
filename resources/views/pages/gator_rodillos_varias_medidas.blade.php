cbb888ffc77560fa288c1d409a5f5ff8e0e28c6d
frkodes <frkalderon@gmail.com>
2018-01-08 17:16:49 -0600

    Another title fix

--- resources/views/pages/gator_rodillos_varias_medidas.blade.php
+++ resources/views/pages/gator_rodillos_varias_medidas.blade.php
@@ -10,10 +10,8 @@
 				<h2>GATOR RODILLOS</h2>
 			</div>
 			<div class="col-xs-12 col-sm-7 col-md-8 empresa-text">
-				<p>Se impulsan con un motor diesel Cummins turboalimentado de 71 kW (95 hp)</p>
-				<p>Estos modelos están disponibles con tracción de eje y son adecuados para una amplia variedad de aplicaciones.</p>
-				<p>Para proyectos más exigentes donde se requiera una capacidad de avance en pendiente superior.</p>
-				<p>Tiene un peso puesto en marcha de  10 toneladas y un ancho de 2.13 Mts.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore dignissimos quisquam vel architecto. Odit, recusandae. Aspernatur illum aperiam eum officiis, maiores ipsam nisi rem iure libero voluptatibus veritatis. Inventore, itaque?</p>
+				
 				<p>&nbsp;</p>
 				<p>&nbsp;</p>
 				<p>&nbsp;</p>
