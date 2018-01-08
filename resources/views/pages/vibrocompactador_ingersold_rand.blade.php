ac6366d591bf58626a7d7ebdd6f23388207fa831
frkodes <frkalderon@gmail.com>
2018-01-08 14:53:55 -0600

    Just a little title fix

--- resources/views/pages/vibrocompactador_ingersold_rand.blade.php
+++ resources/views/pages/vibrocompactador_ingersold_rand.blade.php
@@ -6,8 +6,8 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-5">
-				<h1 class="section-title passion">VIBROCOMPACTADOR INGERSOLD</h1>
-				<h2>RAND</h2>
+				<h1 class="section-title passion">VIBROCOMPACTADOR</h1>
+				<h2>INGERSOLD RAND</h2>
 			</div>
 			<div class="col-xs-12 col-sm-7 empresa-text">
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae cumque ex ratione possimus, error, asperiores, quidem odio aspernatur nobis ducimus quibusdam eveniet cum earum accusamus sunt labore porro nemo nostrum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae cumque ex ratione possimus, error, asperiores, quidem odio aspernatur nobis ducimus quibusdam eveniet cum earum accusamus sunt labore porro nemo nostrum!</p>
