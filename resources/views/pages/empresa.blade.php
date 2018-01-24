67541e94b675d38379ad3d5965b39670de0fe62c
frkodes <frkalderon@gmail.com>
2018-01-24 17:38:44 -0600

    Adding the about us text

--- resources/views/pages/empresa.blade.php
+++ resources/views/pages/empresa.blade.php
@@ -5,11 +5,33 @@
 
 	<div class="container">
 		<div class="row">
-			<h1 class="col-xs-12 col-sm-3 section-title passion">Nuestra empresa</h1>
+			<h1 class="col-xs-12 col-sm-3 section-title passion">¿QUIÉNES SOMOS?</h1>
 			<div class="col-xs-12 col-sm-9 empresa-text">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae cumque ex ratione possimus, error, asperiores, quidem odio aspernatur nobis ducimus quibusdam eveniet cum earum accusamus sunt labore porro nemo nostrum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae cumque ex ratione possimus, error, asperiores, quidem odio aspernatur nobis ducimus quibusdam eveniet cum earum accusamus sunt labore porro nemo nostrum!</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae cumque ex ratione possimus, error, asperiores, quidem odio aspernatur nobis ducimus quibusdam eveniet cum earum accusamus sunt labore porro nemo nostrum!</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae cumque ex ratione possimus, error, asperiores, quidem odio aspernatur nobis ducimus quibusdam eveniet cum earum accusamus sunt labore porro nemo nostrum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae cumque ex ratione possimus, error, asperiores, quidem odio aspernatur nobis ducimus quibusdam eveniet cum earum accusamus sunt labore porro nemo nostrum!</p>
+				<p>Somos una empresa mexicana con mas de 18 años de experiencia en el ramo de la construcción, minería y venta de refacciones para equipos de trituración.</p>
+				<p>Siendo uno de las constructoras mas grandes del estado de Jalisco, Urbanizaciones Gyb se abrió brecha primero en el campo de la trituración de agregados pétreos y posteriormente en la explotación de minas, por lo cual hemos crecido en infraestructura, equipo y experiencia logrando ser una referencia en el ramo a nivel occidente y creciendo a otros lugares de la república.</p>
+				
+				<h2 class="section-title blue passion">VISIÓN</h2>
+				
+				<p>Seguir siendo referente en el occidente, expandirnos en el país con el reconocimiento de nuestro trabajo y seguir siendo una empresa confiable y rentable llevando a cabo proyectos innovadores y sustentables.</p>
+
+				<h2 class="section-title blue passion">MISIÓN</h2>
+				
+				<p>Ser la solución que los clientes están buscando a través de propuestas rentables y desarrollar la relación costo-beneficio asegurando un trabajo de confianza y satisfacción, generando valor financiero y social para las comunidades en las que trabajamos.</p>
+				
+				<h2 class="section-title blue passion">VALORES</h2>
+
+				<ul>
+					<li>Experiencia</li>
+					<li>Compromiso</li>
+					<li>Calidad</li>
+					<li>Servicio </li>
+					<li>Innovación</li>
+					<li>Competitividad</li>
+					<li>Responsabilidad</li>
+				</ul>
+
+				<p>&nbsp;</p>
+
 			</div>
 		</div>
 	</div>
