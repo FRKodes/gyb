7cbd70feaafb3384bdf4cf239a9d090f85f2ec92
frkodes <frkalderon@gmail.com>
2018-01-25 17:04:14 -0600

    Justifying text

--- resources/views/pages/empresa.blade.php
+++ resources/views/pages/empresa.blade.php
@@ -6,7 +6,7 @@
 	<div class="container">
 		<div class="row">
 			<h1 class="col-xs-12 col-sm-3 section-title passion">¿QUIÉNES SOMOS?</h1>
-			<div class="col-xs-12 col-sm-9 empresa-text">
+			<div class="col-xs-12 col-sm-9 empresa-text text-justify">
 				<p>Somos una empresa mexicana con mas de 18 años de experiencia en el ramo de la construcción, minería y venta de refacciones para equipos de trituración.</p>
 				<p>Siendo uno de las constructoras mas grandes del estado de Jalisco, Urbanizaciones Gyb se abrió brecha primero en el campo de la trituración de agregados pétreos y posteriormente en la explotación de minas, por lo cual hemos crecido en infraestructura, equipo y experiencia logrando ser una referencia en el ramo a nivel occidente y creciendo a otros lugares de la república.</p>
 				
