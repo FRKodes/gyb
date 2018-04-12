d0d9abcd6260bfb7fe7cec5f303a8f80420a562c
frkodes <frkalderon@gmail.com>
2018-04-12 17:59:51 -0500

    trying to add the chat link

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,6 +12,9 @@
 		<footer>
 			@include('partials.footer')
 		</footer>
+		{{-- <a target="_blank" class="whatsapp" href="https://api.whatsapp.com/api/send?phone=+5213332015517&text=Hola%20quiero%20info">
+			<img src="/images/whatsapp.png" alt="Mándanos un mensaje por Whatsapp">
+		</a> --}}
 		<script src="js/app.js"></script>
 		<script src="js/slick.js"></script>
 		<script src="js/validate.js"></script>
